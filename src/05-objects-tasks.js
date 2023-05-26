20e23216b3749cfe46a4aebd1134827067efcfbe
fasty86 <oleg.tolm1986@gmail.com>
2023-05-26 16:15:00 +0300

    feat: resolve object tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,10 +20,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  // throw new Error('Not implemented');
+  this.width = width;
+  this.height = height;
 }
 
+Rectangle.prototype.getArea = function () {
+  return this.width * this.height;
+};
 
 /**
  * Returns the JSON representation of specified object
@@ -35,8 +40,9 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  // throw new Error('Not implemented');
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,15 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const obj = JSON.parse(json);
+  const result = Object.create(proto);
+  Object.keys(obj).forEach((key) => {
+    result[key] = obj[key];
+  });
+  // obj.__proto__ = proto;
+  return result;
 }
 
 
@@ -110,36 +123,196 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+// class
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  // path: [],
+  // hier: ['element', 'id'],
+  order: [
+    'element',
+    'id',
+    'class',
+    'attribute',
+    'pseudo-class',
+    'pseudo-element',
+  ],
+  element(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.check('element');
+    that.checkOrder('element');
+    that.path.push({ type: 'element', value });
+    return that;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  combinator(value) {
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    // that.checkOrder('element');
+    that.path.push({ type: 'combinator', value });
+    // that.check();
+    return that;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.check('id');
+    that.checkOrder('id');
+    that.path.push({ type: 'id', value });
+    return that;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.checkOrder('class');
+    that.path.push({ type: 'class', value });
+    // that.check();
+    return that;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.checkOrder('attr');
+    that.path.push({ type: 'attr', value });
+    // that.check();
+    return that;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.checkOrder('pseudoClass');
+    that.path.push({ type: 'pseudoClass', value });
+    // that.check();
+    return that;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    // throw new Error('Not implemented');
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    that.check('pseudoElement');
+    that.path.push({ type: 'pseudoElement', value });
+    return that;
+  },
+
+  combine(selector1, combinator, selector2) {
+    let that = this;
+    that = !this.path ? Object.create(this, { path: { value: [] } }) : this;
+    selector1.path.forEach((element) => {
+      that.path.push(element);
+      return element;
+    });
+    that.path.push({ type: 'combinator', value: combinator });
+    selector2.path.forEach((element) => {
+      that.path.push(element);
+      return element;
+    });
+    return that;
+  },
+  check(prop) {
+    const keys = this.path.map(({ type }) => type);
+    // const test = new Set(keys);
+    if (keys.includes(prop)) {
+      throw new Error(
+        'Element, id and pseudo-element should not occur more then one time inside the selector',
+      );
+    }
+  },
+  checkOrder(prop) {
+    const test = this.path.map(({ type }) => type);
+    let values = [];
+    let isExist = false;
+    switch (prop) {
+      case 'element':
+        if (this.path.length > 0) {
+          throw new Error(
+            'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+          );
+        }
+        break;
+      case 'id':
+        values = ['class', 'attr', 'pseudoClass', 'pseudoElement'];
+        isExist = values.some((val) => test.includes(val));
+        if (isExist) {
+          throw new Error(
+            'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+          );
+        }
+        break;
+      case 'class':
+        values = ['attr', 'pseudoClass', 'pseudoElement'];
+        isExist = values.some((val) => test.includes(val));
+        if (isExist) {
+          throw new Error(
+            'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+          );
+        }
+        break;
+      case 'attr':
+        values = ['pseudoClass', 'pseudoElement'];
+        isExist = values.some((val) => test.includes(val));
+        if (isExist) {
+          throw new Error(
+            'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+          );
+        }
+        break;
+      case 'pseudoClass':
+        values = ['pseudoElement'];
+        isExist = values.some((val) => test.includes(val));
+        if (isExist) {
+          throw new Error(
+            'Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element',
+          );
+        }
+        break;
+      default:
+        break;
+    }
+  },
+  stringify() {
+    let res = '';
+    this.path.map(({ type, value }) => {
+      // console.log(type, value);
+      let val = value;
+      switch (type) {
+        case 'element':
+          break;
+        case 'id':
+          val = `#${val}`;
+          break;
+        case 'class':
+          val = `.${val}`;
+          break;
+        case 'attr':
+          val = `[${val}]`;
+          break;
+        case 'pseudoClass':
+          val = `:${val}`;
+          break;
+        case 'pseudoElement':
+          val = `::${val}`;
+          break;
+        case 'combinator':
+          val = ` ${val} `;
+          break;
+        default:
+          break;
+      }
+      res = `${res}${val}`;
+      return val;
+    });
+    return res;
   },
 };
-
 
 module.exports = {
   Rectangle,
